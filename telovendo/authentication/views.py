80d3c6994cda269a12a337953d009876e07dd599
CynthiaMichelle9 <129108582+CynthiaMichelle9@users.noreply.github.com>
2023-06-15 14:26:26 -0400

    Actualización de logo y templates

--- telovendo/authentication/views.py
+++ telovendo/authentication/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import redirect, render
-from django.views import View
+from django.views.generic import View, CreateView
 from .forms import FormularioLogin
 from django.contrib.auth import logout, login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
 
 # Create your views here.
 class LoginView(View):
@@ -18,7 +20,8 @@ class LoginView(View):
             login(request, usuario)
             return redirect('homeprivado')
         else:
-            context = {"error": "Usuario no encontrado", 'formulario_login': FormularioLogin()}
+            context = {"error": "Usuario no encontrado",
+                       'formulario_login': FormularioLogin()}
             return render(request, 'login.html', context)
         
 class CerrarSesion(View):
